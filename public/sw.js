fcad5f7fdf19296706daeb07babe6b46024473a1
フェリセッタ <zhuw160@gmail.com>
2026-06-16 01:30:33 +0800

    v1.1.13

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rp-toolbox-v4'; 
+const CACHE_NAME = 'rp-toolbox-v5'; 
 
 const ASSETS = [
   './',
@@ -35,27 +35,28 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET' || !event.request.url.startsWith('http')) return;
-
+  let cacheKeyUrl = event.request.url;
   const reqUrl = new URL(event.request.url);
-  let targetUrlStr = event.request.url;
   const pathName = reqUrl.pathname;
+
   if (pathName !== '/' && !pathName.split('/').pop().includes('.')) {
     const cleanPath = pathName.endsWith('/') ? pathName.slice(0, -1) : pathName;
-    targetUrlStr = reqUrl.origin + cleanPath + '.html' + reqUrl.search;
+    cacheKeyUrl = reqUrl.origin + cleanPath + '.html' + reqUrl.search;
   }
+
   event.respondWith(
-    fetch(targetUrlStr)
+    fetch(event.request)
       .then(networkResponse => {
         if (networkResponse && networkResponse.status === 200) {
           const resClone = networkResponse.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(targetUrlStr, resClone));
+          caches.open(CACHE_NAME).then(cache => cache.put(cacheKeyUrl, resClone));
         }
         return networkResponse;
       })
       .catch(() => {
-        return caches.match(targetUrlStr).then(cachedResponse => {
+        return caches.match(cacheKeyUrl).then(cachedResponse => {
           if (cachedResponse) return cachedResponse;
-          console.error('彻底离线且无缓存：', targetUrlStr);
+          console.error('彻底离线且无缓存：', cacheKeyUrl);
           throw new Error('离线且无缓存');
         });
       })
